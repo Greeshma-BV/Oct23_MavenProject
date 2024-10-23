7e174686d79f5d34ded26aa4b8603c2b170f2728
grees <grees@NAINIKA>
2024-10-23 12:31:49 +0530

    Test commit

---
+++ Oct23_MavenProject/src/main/java/OCt23Package/Subtract.java
@@ -0,0 +1,18 @@
+package OCt23Package;
+
+public class Subtract 
+{
+	public void sub(int a, int b)
+	{
+		int c= a-b;
+		System.out.println(c);
+				
+	}
+
+	public static void main(String[] args) 
+	{
+		Subtract su=new Subtract();
+		su.sub(9, 4);
+	}
+
+}
